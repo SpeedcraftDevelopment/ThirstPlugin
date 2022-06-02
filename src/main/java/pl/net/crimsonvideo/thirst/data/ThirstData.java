ac38e86d9991e057db24e51fb2f9305f8a871045
Kuba663 <kuba63b@gmail.com>
2022-06-02 19:57:09 +0200

    Created ThirstData.java

---
+++ src/main/java/pl/net/crimsonvideo/thirst/data/ThirstData.java
@@ -0,0 +1,11 @@
+package pl.net.crimsonvideo.thirst.data;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.Serializable;
+
+public class ThirstData implements Serializable {
+    private transient static final long serialVersionUID = 7410964373687906258L;
+
+    private transient JavaPlugin plugin;
+}
