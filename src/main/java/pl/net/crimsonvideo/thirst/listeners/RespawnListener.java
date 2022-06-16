ae5d0612679934745e92d2f76bc7f6ca28f3b0c2
Kuba663 <kuba63b@gmail.com>
2022-06-16 12:23:28 +0200

    made respawn task async

--- src/main/java/pl/net/crimsonvideo/thirst/listeners/RespawnListener.java
+++ src/main/java/pl/net/crimsonvideo/thirst/listeners/RespawnListener.java
@@ -3,13 +3,26 @@ package pl.net.crimsonvideo.thirst.listeners;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 import pl.net.crimsonvideo.thirst.Thirst;
 
 public class RespawnListener implements Listener {
 
+    public final JavaPlugin plugin;
+
+    public RespawnListener(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
     @EventHandler
     public void onPlayerRespawning(@NotNull PlayerRespawnEvent event) {
-        Thirst.getAPI().hydrationAPI.setHydration(event.getPlayer(),20);
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                Thirst.getAPI().hydrationAPI.setHydration(event.getPlayer(),20);
+            }
+        }.runTaskAsynchronously(plugin);
     }
 }
