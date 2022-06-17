43a3a9e5e04de0c6483cb87a277d4cae3e2b2fce
Kuba663 <kuba63b@gmail.com>
2022-06-17 09:45:13 +0200

    Added IThirstData interface.

---
+++ src/main/java/pl/net/crimsonvideo/thirst/data/IThirstData.java
@@ -0,0 +1,19 @@
+package pl.net.crimsonvideo.thirst.data;
+
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import pl.net.crimsonvideo.thirst.exceptions.ValueTooHighError;
+import pl.net.crimsonvideo.thirst.exceptions.ValueTooLowError;
+
+import java.util.UUID;
+
+public interface IThirstData {
+    void setPlayerHydration(@NotNull Player p, float hydration);
+    void setPlayerHydration(@NotNull UUID p, float hydration);
+    float getPlayerHydration(@NotNull Player p) throws IndexOutOfBoundsException;
+    float getPlayerHydration(@NotNull UUID p) throws IndexOutOfBoundsException;
+    void addHydration(@NotNull Player p, float hydration) throws IndexOutOfBoundsException, ValueTooHighError, ValueTooLowError;
+    void addHydration(@NotNull UUID p, float hydration) throws IndexOutOfBoundsException, ValueTooHighError, ValueTooLowError;
+    void subtractHydration(@NotNull Player p, float hydration) throws IndexOutOfBoundsException, ValueTooLowError, ValueTooHighError;
+    void subtractHydration(@NotNull UUID p, float hydration) throws IndexOutOfBoundsException, ValueTooLowError, ValueTooHighError;
+}
