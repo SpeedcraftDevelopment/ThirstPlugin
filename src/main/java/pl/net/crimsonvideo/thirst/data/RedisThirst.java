1a469ef524e55965216aea1ced0f1b0e91dc8432
Kuba663 <kuba63b@gmail.com>
2022-06-17 11:14:23 +0200

    Added exceptions to RedisThirst.

--- src/main/java/pl/net/crimsonvideo/thirst/data/RedisThirst.java
+++ src/main/java/pl/net/crimsonvideo/thirst/data/RedisThirst.java
@@ -36,7 +36,9 @@ public record RedisThirst(JavaPlugin plugin, JedisPool pool) implements IThirstD
         try (Jedis jedis = pool.getResource()) {
             if (plugin.getConfig().isString("redis.auth.username") && plugin.getConfig().isString("redis.auth.password"))
                 jedis.auth(plugin.getConfig().getString("redis.auth.username"), plugin.getConfig().getString("redis.auth.password"));
-            return Float.parseFloat(jedis.get(p.getUniqueId().toString()));
+            if (jedis.exists(p.getUniqueId().toString()))
+                return Float.parseFloat(jedis.get(p.getUniqueId().toString()));
+            else throw new IndexOutOfBoundsException("No player in database.");
         }
     }
 
@@ -45,43 +47,61 @@ public record RedisThirst(JavaPlugin plugin, JedisPool pool) implements IThirstD
         try (Jedis jedis = pool.getResource()) {
             if (plugin.getConfig().isString("redis.auth.username") && plugin.getConfig().isString("redis.auth.password"))
                 jedis.auth(plugin.getConfig().getString("redis.auth.username"), plugin.getConfig().getString("redis.auth.password"));
-            return Float.parseFloat(jedis.get(p.toString()));
+            if (jedis.exists(p.toString()))
+                return Float.parseFloat(jedis.get(p.toString()));
+            else throw new IndexOutOfBoundsException("No player in database.");
         }
     }
 
     @Override
     public void addHydration(@NotNull Player p, float hydration) throws IndexOutOfBoundsException, ValueTooHighError, ValueTooLowError {
+        if (hydration > 20) throw new ValueTooHighError("Value added higher than 20");
+        else if (hydration < 0) throw new ValueTooLowError("Value subtracted lower than 0");
         try (Jedis jedis = pool.getResource()) {
             if (plugin.getConfig().isString("redis.auth.username") && plugin.getConfig().isString("redis.auth.password"))
                 jedis.auth(plugin.getConfig().getString("redis.auth.username"), plugin.getConfig().getString("redis.auth.password"));
-            jedis.set(p.getUniqueId().toString(),Float.toString(Float.parseFloat(jedis.get(p.getUniqueId().toString()))+hydration));
+            if (jedis.exists(p.getUniqueId().toString()))
+                jedis.set(p.getUniqueId().toString(),Float.toString(Float.parseFloat(jedis.get(p.getUniqueId().toString()))+hydration));
+            else throw new IndexOutOfBoundsException("No player in database.");
         }
     }
 
     @Override
     public void addHydration(@NotNull UUID p, float hydration) throws IndexOutOfBoundsException, ValueTooHighError, ValueTooLowError {
+        if (hydration > 20) throw new ValueTooHighError("Value added higher than 20");
+        else if (hydration < 0) throw new ValueTooLowError("Value subtracted lower than 0");
         try (Jedis jedis = pool.getResource()) {
             if (plugin.getConfig().isString("redis.auth.username") && plugin.getConfig().isString("redis.auth.password"))
                 jedis.auth(plugin.getConfig().getString("redis.auth.username"), plugin.getConfig().getString("redis.auth.password"));
-            jedis.set(p.toString(),Float.toString(Float.parseFloat(jedis.get(p.toString()))+hydration));
+            if (jedis.exists(p.toString()))
+                jedis.set(p.toString(),Float.toString(Float.parseFloat(jedis.get(p.toString()))+hydration));
+            else throw new IndexOutOfBoundsException("No player in database.");
         }
     }
 
     @Override
     public void subtractHydration(@NotNull Player p, float hydration) throws IndexOutOfBoundsException, ValueTooLowError, ValueTooHighError {
+        if (hydration > 20) throw new ValueTooHighError("Value added higher than 20");
+        else if (hydration < 0) throw new ValueTooLowError("Value subtracted lower than 0");
         try (Jedis jedis = pool.getResource()) {
             if (plugin.getConfig().isString("redis.auth.username") && plugin.getConfig().isString("redis.auth.password"))
                 jedis.auth(plugin.getConfig().getString("redis.auth.username"), plugin.getConfig().getString("redis.auth.password"));
-            jedis.set(p.getUniqueId().toString(),Float.toString(Float.parseFloat(jedis.get(p.getUniqueId().toString()))-hydration));
+            if (jedis.exists(p.getUniqueId().toString()))
+                jedis.set(p.getUniqueId().toString(),Float.toString(Float.parseFloat(jedis.get(p.getUniqueId().toString()))-hydration));
+            else throw new IndexOutOfBoundsException("No player in database.");
         }
     }
 
     @Override
     public void subtractHydration(@NotNull UUID p, float hydration) throws IndexOutOfBoundsException, ValueTooLowError, ValueTooHighError {
+        if (hydration > 20) throw new ValueTooHighError("Value added higher than 20");
+        else if (hydration < 0) throw new ValueTooLowError("Value subtracted lower than 0");
         try (Jedis jedis = pool.getResource()) {
             if (plugin.getConfig().isString("redis.auth.username") && plugin.getConfig().isString("redis.auth.password"))
                 jedis.auth(plugin.getConfig().getString("redis.auth.username"), plugin.getConfig().getString("redis.auth.password"));
-            jedis.set(p.toString(),Float.toString(Float.parseFloat(jedis.get(p.toString()))-hydration));
+            if (jedis.exists(p.toString()))
+                jedis.set(p.toString(),Float.toString(Float.parseFloat(jedis.get(p.toString()))-hydration));
+            else throw new IndexOutOfBoundsException("No player in database.");
         }
     }
 }
