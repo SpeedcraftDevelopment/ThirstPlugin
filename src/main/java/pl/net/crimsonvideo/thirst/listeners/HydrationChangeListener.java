a14f538ffca81b080ecde83f8730c9901a1f3d00
Kuba663 <kuba63b@gmail.com>
2022-06-17 09:09:53 +0200

    Replaced classes with records.

--- src/main/java/pl/net/crimsonvideo/thirst/listeners/HydrationChangeListener.java
+++ src/main/java/pl/net/crimsonvideo/thirst/listeners/HydrationChangeListener.java
@@ -10,21 +10,15 @@ import pl.net.crimsonvideo.thirst.Localisation;
 import pl.net.crimsonvideo.thirst.Thirst;
 import pl.net.crimsonvideo.thirst.events.HydrationChangedEvent;
 
-@API(status= API.Status.INTERNAL,since="0.2-SNAPSHOT")
-public class HydrationChangeListener implements Listener {
-
-    private final JavaPlugin plugin;
-
-    public HydrationChangeListener(JavaPlugin plugin) {
-        this.plugin = plugin;
-    }
+@API(status = API.Status.INTERNAL, since = "0.2-SNAPSHOT")
+public record HydrationChangeListener(JavaPlugin plugin) implements Listener {
 
     @EventHandler
-    public void onHydrationChanged(@NotNull HydrationChangedEvent event){
+    public void onHydrationChanged(@NotNull HydrationChangedEvent event) {
         if (event.isCancelled())
             return;
-        if (plugin.getConfig().getBoolean("log",false))
-            plugin.getLogger().info(String.format("%s's hydration changed by %.5f", event.getPlayer().getName(),event.getChange()));
-        event.getPlayer().sendActionBar(ChatColor.DARK_BLUE + new String(new char[((int) Math.floor(Thirst.getAPI().hydrationAPI.getHydration(event.getPlayer())))]).replace('\0', '|') + ChatColor.BLACK + new String(new char[(20-((int) Math.floor(Thirst.getAPI().hydrationAPI.getHydration(event.getPlayer()))))]).replace('\0','|') + ChatColor.WHITE + " - " + Localisation.getLocalisationForPlayer(event.getPlayer()).getLocalisedString("thirst.actionbar.hydration"));
+        if (plugin.getConfig().getBoolean("log", false))
+            plugin.getLogger().info(String.format("%s's hydration changed by %.5f", event.getPlayer().getName(), event.getChange()));
+        event.getPlayer().sendActionBar(ChatColor.DARK_BLUE + new String(new char[((int) Math.floor(Thirst.getAPI().hydrationAPI.getHydration(event.getPlayer())))]).replace('\0', '|') + ChatColor.BLACK + new String(new char[(20 - ((int) Math.floor(Thirst.getAPI().hydrationAPI.getHydration(event.getPlayer()))))]).replace('\0', '|') + ChatColor.WHITE + " - " + Localisation.getLocalisationForPlayer(event.getPlayer()).getLocalisedString("thirst.actionbar.hydration"));
     }
 }
